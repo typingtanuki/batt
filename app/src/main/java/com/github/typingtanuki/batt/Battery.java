191a42fbeb25ce81da215ad37bbe005c75f1326a
Clerc Mathias <clerc@infoscience.co.jp>
2020-11-09 10:08:41 +0900

    Initial commit

---
+++ app/src/main/java/com/github/typingtanuki/batt/Battery.java
@@ -0,0 +1,77 @@
+package com.github.typingtanuki.batt;
+
+public class Battery {
+    private String url;
+    private double volt;
+    private int amp;
+    private Double watt;
+    private String description;
+    private String model;
+
+    public Battery(String url, double volt, int amp, Double watt) {
+        super();
+
+        this.url = url;
+        this.volt = volt;
+        this.amp = amp;
+        this.watt = watt;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public double getVolt() {
+        return volt;
+    }
+
+    public void setVolt(double volt) {
+        this.volt = volt;
+    }
+
+    public int getAmp() {
+        return amp;
+    }
+
+    public void setAmp(int amp) {
+        this.amp = amp;
+    }
+
+    public Double getWatt() {
+        return watt;
+    }
+
+    public void setWatt(Double watt) {
+        this.watt = watt;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String toString() {
+        return "* " + model + "\r\n" +
+                "** " + description + "\r\n" +
+                "** " + url + "\r\n" +
+                "** " + volt + "V\r\n" +
+                "** " + amp + "mAh\r\n" +
+                "** " + watt + "W";
+    }
+}
---
+++ app/src/main/java/com/github/typingtanuki/batt/BatteryComparator.java
@@ -0,0 +1,16 @@
+package com.github.typingtanuki.batt;
+
+public class BatteryComparator implements java.util.Comparator<Battery> {
+    @Override
+    public int compare(Battery o1, Battery o2) {
+        int amp = Integer.compare(o1.getAmp(), o2.getAmp());
+        if (amp != 0) {
+            return -amp;
+        }
+        int watt = Double.compare(o1.getWatt(), o2.getWatt());
+        if (watt != 0) {
+            return -watt;
+        }
+        return o1.getModel().compareTo(o2.getModel());
+    }
+}
